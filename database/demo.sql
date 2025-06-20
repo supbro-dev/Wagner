73461edb132163b27990f7ac21b58e1f483e5526
supbro <bigbullyrn@gmail.com>
2025-06-20 10:44:31 +0800

    修改数据库配置和初始化sql

--- database/demo.sql
+++ database/demo.sql
@@ -1,9 +1,2 @@
--- 初始化工作点
-INSERT INTO workplace (code,
-                       name,
-                       region_code,
-                       industry_code)
-VALUES ('workplace1', '1号工作点', 'NORTH', 'FOOD');
-
--- 初始化员工
+-- 员工当天工作
 
